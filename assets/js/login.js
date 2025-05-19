b8d0e3a3b3177312bc0b3f56d09ae9a32ce869d9
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-05-20 00:55:00 +0330

    section 3 and 5 updated

---
+++ assets/js/login.js
@@ -0,0 +1,7 @@
+// login page
+
+ document.getElementById('termsLink').addEventListener('click', function(e) {
+    e.preventDefault(); // جلوگیری از رفتار پیش‌فرض لینک
+    // می‌توانید اینجا کد دیگری برای نمایش شرایط و قوانین اضافه کنید
+    console.log('لینک شرایط و قوانین کلیک شد، اما چک‌باکس تغییر نکرد');
+  });
