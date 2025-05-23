efc9cd8c52210eb5e23644bbf288bd54ff22cb2c
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-05-23 15:09:59 +0330

    first commit

--- assets/js/wellcome.js
+++ assets/js/wellcome.js
@@ -1,3 +1,3 @@
 setTimeout(function () {
-    window.location.href = "steps.html"; // تغییر مسیر به صفحه steps.html بعد از ۳ ثانیه
+    window.location.href = "index.html"; // تغییر مسیر به صفحه steps.html بعد از ۳ ثانیه
 }, 3000); // 3000 میلی‌ثانیه = ۳ ثانیه
